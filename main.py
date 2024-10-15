97ec5839e7c5313b4bebd7db8e2d8d75943edf21
bend91 <benowendraper@gmail.com>
2024-10-15 17:53:20 +0100

    2024-10-15_GUI work

--- main.py
+++ main.py
@@ -1,58 +1,51 @@
-from PySide6.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton
+from PySide6.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton, QLabel
 import sys
-sys.path.append("/Users/benjamindraper/GitHub/ULA/Plasmid_Editor")
-from ScreeningDigest_tk.functions import *
-
-# Only needed for access to command line arguments
-import sys
+from widgets import *
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 # If you know you won't use command line arguments QApplication([]) works too.
 
 
-def MainApplication(QApplication):
+class MainApplication(QApplication):
     def __init__(self, *args, **kwargs):
-        super(self, QApplication).__init__(*args, **kwargs)
-
-
-def MainWindow(QWidget):
-    def __init__(self, *args, **kwargs):
-        # QWidget.__init__(self, *args, **kwargs)
-        layout = QVBoxLayout()
-        h_layout = QHBoxLayout()
-
-        text_box1 = QTextEdit()
-        text_box2 = QTextEdit()
-        for widget in [text_box1, text_box2]:
-            h_layout.addWidget(widget)
-        submit_button = QPushButton("Submit")
-        layout.addLayout(h_layout)
-        layout.addWidget(submit_button)
-        self.setLayout(layout)
+        QApplication.__init__(self)
+        # super(self, QApplication).__init__(*args, **kwargs)
 
 
 def main():
     main_app = MainApplication(sys.argv)
+    # main_app = QApplication(sys.argv)
 
-# Create a Qt widget, which will be our window.
-    window = MainWindow()
+
+    window = MainWidget()
+    # window = QWidget()
     # layout = QVBoxLayout()
     # h_layout = QHBoxLayout()
+    # v_layout1 = QVBoxLayout()
+    # v_layout2 = QVBoxLayout()
     
+    # label1 = QLabel("Enter Sequence 1")
+    # label2 = QLabel("Enter Sequence 2")
     # text_box1 = QTextEdit()
     # text_box2 = QTextEdit()
 
-    # h_layout.addWidget(text_box1)
-    # h_layout.addWidget(text_box2)
+    # v_layout1.addWidget(label1)
+    # v_layout1.addWidget(text_box1)
+
+    # v_layout2.addWidget(label2)
+    # v_layout2.addWidget(text_box2)
 
     # submit_button = QPushButton("Submit")
 
-    # layout.addWidget(text_box)
+    # h_layout.addLayout(v_layout1)
+    # h_layout.addLayout(v_layout2)
+
+    # layout.addLayout(h_layout)
+    # layout.addWidget(submit_button)
     # window.setLayout(layout)
     window.show()
     main_app.exec()
-
 
 if __name__ == "__main__":
     main()
